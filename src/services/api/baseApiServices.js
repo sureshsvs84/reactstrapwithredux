c1e948c65034e95352004e444cffe591caabff49
sureshsvs84 <suresh.s@smnetserv.com>
2019-09-01 00:40:39 +0530

    cross domain issue fixed

--- src/services/api/baseApiServices.js
+++ src/services/api/baseApiServices.js
@@ -4,6 +4,10 @@ import { configuration } from '../../appConfig';
 axios.defaults.baseURL = configuration.apiBaseUrl;
 //axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.common['Access-Control-Allow-Origin']= '*';
+axios.defaults.headers.common['Access-Control-Allow-Methods']='GET, PUT, POST, DELETE, OPTIONS';
+axios.defaults.headers.common['Access-Control-Allow-Headers']='Content-Type';
+axios.defaults.headers.get['Content-Type'] = 'application/json';
+
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.headers.post['Acces-Control-Allow-Origin'] = '*';
@@ -18,17 +22,15 @@ const param = {}
 * @param {*} config 
 */
 export const postData = async (url, config) => {
-    axios.post(url, config.data)
-        .then((response) => {
-            if (response && response.status === 200 && response.statusText === 'OK') {
-                console.log(response);
-                return response.result;
-            }
-
-        }, (error) => {
-            console.log(error);
-            return error;
-        });
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.defaults.headers.post['Accept'] = 'application/json';
+    axios.defaults.headers.post['Acces-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+    const response = await axios.post(url, config.data);
+    if (response && response.status === 200) {
+        return response.data;
+    }
+    return response;
 }
 /**
 * @param {*} url 
@@ -48,3 +50,17 @@ export const getData = async (url, config) => {
         });
     return response;
 }
+
+export const CreateData = async (url, config) => {
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.defaults.headers.post['Accept'] = 'application/json';
+    axios.defaults.headers.post['Acces-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.put['Content-Type'] = 'application/json';
+    axios.defaults.headers.put['Accept'] = 'application/json';
+    axios.defaults.headers.put['Acces-Control-Allow-Origin'] = '*';
+    const response = await axios.put(url, config.data);
+    if (response.status === 200 && response.statusText === 'OK') {
+        return response.data;
+    }
+    return response;
+};
